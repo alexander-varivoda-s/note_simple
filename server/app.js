4d851638fe7d662a2468a35663eaf352ebb9117b
alexander.varivoda.s <alexander.varivoda.s@gmail.com>
2019-04-10 16:06:07 +0300

    Implemented error handing middleware for Koa app

--- server/app.js
+++ server/app.js
@@ -1,5 +1,9 @@
 const Koa = require('koa');
 
+const errorsMiddleware = require('./middleware/errors');
+
 const app = new Koa();
+
+app.use(errorsMiddleware());
 
 module.exports = app;
---
+++ server/middleware/errors.js
@@ -0,0 +1,32 @@
+/**
+ * Errors handing middleware
+ */
+module.exports = function middleware() {
+  return async (ctx, next) => {
+    try {
+      await next();
+    } catch (e) {
+      const errors = [];
+      if (e.name === 'ValidationError') {
+        Object.keys(e.errors).forEach(fieldName => {
+          errors.push({
+            type: 'error',
+            message: e.errors[fieldName].message,
+            field: fieldName,
+          });
+        });
+
+        ctx.status = 400;
+      } else {
+        ctx.status = e.status || 500;
+        errors.push({
+          type: 'error',
+          message: e.status === 500 ? 'Internal server error' : e.message,
+        });
+      }
+
+      ctx.body = errors;
+      ctx.app.emit('error', e, ctx);
+    }
+  };
+};
