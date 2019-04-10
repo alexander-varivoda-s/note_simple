ac3e3b79cf615b6dc651993faadaf1e22c382458
alexander.varivoda.s <alexander.varivoda.s@gmail.com>
2019-04-10 16:27:35 +0300

    Installed @koa/cors@2 and specified origin, allowed methods and headers

--- server/app.js
+++ server/app.js
@@ -1,9 +1,23 @@
 const Koa = require('koa');
+const cors = require('@koa/cors');
 
 const errorsMiddleware = require('./middleware/errors');
 
 const app = new Koa();
 
 app.use(errorsMiddleware());
+app.use(
+  cors({
+    origin: '*',
+    allowMethods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowHeader: [
+      'Origin',
+      'X-Request-With',
+      'Content-Type',
+      'Authorization',
+      'Accept',
+    ],
+  }),
+);
 
 module.exports = app;
