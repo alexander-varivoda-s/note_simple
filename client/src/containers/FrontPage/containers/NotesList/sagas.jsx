589f1e03265487df85aaf92fa648a4e565b4178a
alexander.varivoda.s <alexander.varivoda.s@gmail.com>
2019-06-18 15:25:23 +0300

    Implemented note selection when note pinned/unpinned.

--- client/src/containers/FrontPage/containers/NotesList/sagas.jsx
+++ client/src/containers/FrontPage/containers/NotesList/sagas.jsx
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import { notesAPI } from '../../../../api';
 import {
   PIN_FAILURE,
@@ -6,13 +6,26 @@ import {
   UNPIN_FAILURE,
   UNPIN_SUCCEEDED,
 } from './constants';
+import { getSelectedNoteId } from '../../selectors';
+import { selectNoteAction } from './actions';
+
+function* selectNote(noteId) {
+  const selectedNoteId = yield select(getSelectedNoteId);
+
+  if (selectedNoteId && selectedNoteId !== noteId) {
+    yield put(selectNoteAction(noteId));
+  }
+}
 
 export function* pinNote(action) {
   const { id } = action.payload;
 
   try {
-    const { data: { note } } = yield call(notesAPI.pinNote, { id }, { withCredentials: true });
+    const {
+      data: { note },
+    } = yield call(notesAPI.pinNote, { id }, { withCredentials: true });
     yield put({ type: PIN_SUCCEEDED, payload: { note } });
+    yield selectNote(id);
   } catch (e) {
     yield put({ type: PIN_FAILURE, error: e });
   }
@@ -22,8 +35,11 @@ export function* unpinNote(action) {
   const { id } = action.payload;
 
   try {
-    const { data: { note } } = yield call(notesAPI.unpinNote, { id }, { withCredentials: true });
+    const {
+      data: { note },
+    } = yield call(notesAPI.unpinNote, { id }, { withCredentials: true });
     yield put({ type: UNPIN_SUCCEEDED, payload: { note } });
+    yield selectNote(id);
   } catch (e) {
     yield put({ type: UNPIN_FAILURE, error: e });
   }
