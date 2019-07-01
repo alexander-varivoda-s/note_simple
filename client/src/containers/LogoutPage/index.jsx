3ce13ffa4178c0d44c81bf0c3461cf912c1339ea
alexander.varivoda.s <alexander.varivoda.s@gmail.com>
2019-07-01 11:35:22 +0300

    Rewrote LoginPage component to use react hooks.

--- client/src/containers/LogoutPage/index.jsx
+++ client/src/containers/LogoutPage/index.jsx
@@ -1,28 +1,12 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
 import logout from './actions';
 
-class LogoutPage extends PureComponent {
-  static propTypes = {
-    signOut: PropTypes.func.isRequired,
-  };
+export default function LogoutPage() {
+  const dispatch = useDispatch();
+  dispatch(logout());
 
-  componentDidMount() {
-    const { signOut } = this.props;
-    signOut();
-  }
-
-  render() {
-    return <Redirect to='/login' />;
-  }
+  return <Redirect to='/login' />;
 }
-
-const mapDispatchToProps = dispatch => ({ signOut: () => dispatch(logout()) });
-
-export default connect(
-  null,
-  mapDispatchToProps,
-)(LogoutPage);
