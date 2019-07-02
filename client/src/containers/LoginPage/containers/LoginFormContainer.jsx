5307416493467619a623f2df883f04e33722eab3
alexander.varivoda.s <alexander.varivoda.s@gmail.com>
2019-07-02 11:30:28 +0300

    Rewrote LoginForm and ResetPasswordForm to use hooks.

--- client/src/containers/LoginPage/containers/LoginFormContainer.jsx
+++ client/src/containers/LoginPage/containers/LoginFormContainer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Formik } from 'formik';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { object, string } from 'yup';
 
 import LoginForm from '../components/LoginForm';
@@ -21,37 +20,24 @@ const validationSchema = object().shape({
     .required('Password is required!'),
 });
 
-const handleSubmit = signIn => (values, formikBag) => {
-  const payload = {
-    params: values,
-    onSuccess: () => formikBag.setSubmitting(false),
-    onFailure: () => {
-      formikBag.setSubmitting(false);
-    },
-  };
+export default function LoginFormContainer() {
+  const dispatch = useDispatch();
 
-  signIn(payload);
-};
+  function submitHandler(values, formikBag) {
+    const payload = {
+      params: values,
+      onSuccess: () => formikBag.setSubmitting(false),
+      onFailure: () => formikBag.setSubmitting(false),
+    };
 
-function LoginFormContainer(props) {
-  const { signIn } = props;
+    dispatch(login(payload));
+  }
   return (
     <Formik
       render={LoginForm}
       initialValues={initialValues}
       validationSchema={validationSchema}
-      onSubmit={handleSubmit(signIn)}
+      onSubmit={submitHandler}
     />
   );
 }
-
-LoginFormContainer.propTypes = {
-  signIn: PropTypes.func.isRequired,
-};
-
-const mapDispatchToProps = dispatch => ({ signIn: payload => dispatch(login(payload)) });
-
-export default connect(
-  null,
-  mapDispatchToProps,
-)(LoginFormContainer);
